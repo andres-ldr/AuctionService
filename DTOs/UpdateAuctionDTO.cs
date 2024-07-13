21d03410646b22a779bbaf75f91e9084f6564af0
Andres <lopez.andresrigoberto@gmail.com>
2024-07-12 22:39:10 -0700

    add DTO for auction

---
+++ DTOs/UpdateAuctionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AuctionService.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+public class UpdateAuctionDTO
+{
+    public string Status { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+}
