14d0ec798799919db933f5bc6965735e6df4ba61
Andres <lopez.andresrigoberto@gmail.com>
2024-07-21 21:24:02 -0700

    add: config outbox

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Make conncetion to RabbitMQ and configure endpoints
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
